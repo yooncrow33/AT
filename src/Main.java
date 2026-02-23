51eaeb281fae25e5093f3ebccb8669f42b54752b
yooncrow33 <yooncrow@naver.com>
2026-02-23 16:27:19 +0900

    scope framework activated.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import scope.SideScrollBase;
+
+public class Main extends SideScrollBase {
+    @Override
+    protected void update(double v) {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    public Main(String title, int worldWidth, int worldHeight) {
+        super(title, worldWidth, worldHeight);
+    }
+}
